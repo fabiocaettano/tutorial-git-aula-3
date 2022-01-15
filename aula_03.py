52378d8d95e29814bb398f6c174723c05e2ebe13
fabiocaettano <fabio.caettano74@gmail.com>
2022-01-15 20:56:07 -0300

    funcao multiplicar

--- aula_03.py
+++ aula_03.py
@@ -7,6 +7,10 @@ def subtrair(a,b):
 def soma(a,b):
     print(f"{a} + {b} = {a+b}")
 
+def multiplicar(a,b):
+    print(f"{a} * {b} = {a*b}")
+
 divide(10,5)
 soma(10,5)
 subtrair(10,5)
+multiplicar(10,5)

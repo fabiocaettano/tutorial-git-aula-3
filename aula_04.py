1ecb6399db7c930e61d03d61923b1110d20bd5e0
fabiocaettano <fabio.caettano74@gmail.com>
2022-01-15 21:07:29 -0300

    funcao potencia

--- aula_04.py
+++ aula_04.py
@@ -1,1 +1,12 @@
-print("oi")
+def potencia(a,b):
+    
+    potencia = 1
+    count = 1
+
+    while count <= b:
+        potencia *= a
+        count = count + 1
+
+    print(f"{a} ^ {b} = {potencia}")
+
+potencia(10,5)

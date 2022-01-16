3b07f6576372471989acffb3a41f61a092430247
fabiocaettano <fabio.caettano74@gmail.com>
2022-01-15 21:14:19 -0300

    funcao potencia

--- aula_03.py
+++ aula_03.py
@@ -10,7 +10,19 @@ def soma(a,b):
 def multiplicar(a,b):
     print(f"{a} * {b} = {a*b}")
 
+def potencia(a,b):
+    
+    potencia = 1
+    count = 1
+
+    while count <= b:
+        potencia *= a
+        count = count + 1
+
+    print(f"{a} ^ {b} = {potencia}")
+
 divide(10,5)
 soma(10,5)
 subtrair(10,5)
 multiplicar(10,5)
+potencia(10,5)
